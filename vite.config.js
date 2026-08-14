1d9bb909b8ab18c35e6bba678e9495f8aeacdb11
navigatingnc <39036452+navigatingnc@users.noreply.github.com>
2026-08-14 11:22:36 -0400

    Refactor alias resolution in vite.config.js
    Updated the alias resolution to use fileURLToPath for better compatibility.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
